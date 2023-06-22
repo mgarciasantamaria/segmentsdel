67b48f31cf0a9aff26ce8e29b9de72e337cc626d
Mario Garcia <mandres_garcia@hotmail.com>
2023-06-22 10:09:51 -0500

    Cabio de asunto del correo

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ if Flag_Status('r'):
     psql_db.commit()
     psql_cursor.close()
     psql_db.close()
-    Send_Mail(text_mail, 'cdnbytes summary')
+    Send_Mail(text_mail, 'segments delete summary')
 else:
     Send_Mail('etltoolbox application failure not recognized', 'etltoolbox application failure not recognized')
