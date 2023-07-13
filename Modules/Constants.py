d29eca6401a036552e9f1705dc33a12b0eca7766
Mario Garcia <mandres_garcia@hotmail.com>
2023-07-13 12:37:23 -0500

    Se modifica el destino de la base de datos

--- Modules/Constants.py
+++ Modules/Constants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 #_*_ codig: utf8 _*_
 json_path='../json/vars.json'
-db_data_connect="host=10.10.130.38 dbname=cdndb user=vodtransfers3 password=vod-2022"
+db_data_connect="host=10.10.130.152 dbname=toolboxprod user=vodaplications password=V0D-20234pl1c4t10ns"
 Mail_to=['ingenieriavcmc@vcmedios.com.co', 'Gtecnicovcmc@vcmedios.com.co']
